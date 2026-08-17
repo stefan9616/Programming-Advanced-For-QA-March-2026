d11d7c1837631676ea7c58963d10cedaa7ce0cab
Stefan Djigaliev <143064670+stefan9616@users.noreply.github.com>
2026-08-17 23:08:22 +0300

    Add files via upload

---
+++ 03.Objects-And-Classes-Excercise/02.Article/Program.cs
@@ -0,0 +1,71 @@
+﻿
+string input = Console.ReadLine();
+
+string[] articleInfo = input.Split(", ").ToArray();
+
+string title = articleInfo[0];
+string content = articleInfo[1];
+string author = articleInfo[2];
+
+Article myArticle = new Article(title, content, author);
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] arguments = Console.ReadLine().Split(": ").ToArray();
+
+    string command = arguments[0];
+    string token = arguments[1];
+
+    if(command == "Edit")
+    {
+        myArticle.Edit(token);
+    }
+    else if(command == "ChangeAuthor")
+    {
+        myArticle.ChangeAuthor(token);
+    }
+    else if(command == "Rename")
+    {
+        myArticle.Rename(token);
+    }
+    
+}
+Console.WriteLine(myArticle.ToString());
+
+
+class Article
+{
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+    
+}
---
+++ 03.Objects-And-Classes-Excercise/03.Objects-And-Classes-Excercise/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Diagnostics;
+
+int n = int.Parse(Console.ReadLine());
+
+List<Student> listOfStudents = new List<Student>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] studentInfo = Console.ReadLine().Split(" ").ToArray();
+
+    string firstName = studentInfo[0];
+    string lastName = studentInfo[1];
+    double grade = double.Parse(studentInfo[2]);
+
+    Student currentStudent = new Student(firstName, lastName, grade);
+
+    listOfStudents.Add(currentStudent);
+    
+}
+foreach (var student in listOfStudents.OrderByDescending(s => s.Grade))
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+}
+
+
+class Student
+{
+    public Student(string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    
+    public double Grade { get; set; }
+}
