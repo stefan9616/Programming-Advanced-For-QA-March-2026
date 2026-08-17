ff13c58b5cbcb7432f698482903c8e4fd19a6e5c
Stefan Djigaliev <143064670+stefan9616@users.noreply.github.com>
2026-08-17 23:14:21 +0300

    Add files via upload

---
+++ 05.Exception-Handling-Lab/05-Exception-Handlig-Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿
+try
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number < 0)
+    {
+        throw new Exception("Invalid number.");
+    }
+    else
+    {
+        Console.WriteLine(Math.Sqrt(number));
+    }
+}
+catch(Exception)
+{
+        Console.WriteLine("Invalid number.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
